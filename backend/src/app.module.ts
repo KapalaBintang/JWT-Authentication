5007a369e8a492b8998f3a5cb26e5278cf1b7e9a
Abdul Aziz <bintangkapala@gmail.com>
2024-12-28 17:34:31 +0700

    feat(API):add authentication guard

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, DbModule],
+  imports: [UsersModule, DbModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
